28805271b09b67a47880e11955e833465b7bc437
Shlok Chorghe <shlok4online@gmail.com>
2026-06-14 14:13:51 +0530

    Create leap_year.py

---
+++ Day-10-Functions-with-return/leap_year.py
@@ -0,0 +1,8 @@
+def is_leap_year(year):
+    # Write your code here. 
+    # Don't change the function name.
+    if (year%4==0 and year%100!=0) or year%400==0 :
+        return True
+    else:
+        return False
+is_leap_year(2016)
